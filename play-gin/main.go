911b79ebc86292f05680350bec77a2c869ea45f1
acker <acker.liu@topibd.net>
2020-09-08 01:10:43 +0800

    server reload

--- play-gin/main.go
+++ play-gin/main.go
@@ -6,19 +6,49 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
 	"runtime"
+	"syscall"
+	"time"
+	"context"
 
 	"github.com/gin-gonic/gin"
 )
 
-const dst = "./upgrade_pkg"
+const (
+	VER = "v0.0.12"
+	dst = "./upgrade_pkg"
+)
 
 func main() {
 	g := gin.Default()
 	g.GET("/version", version)
 	g.POST("/home", home)
 	g.POST("/upload", upload)
-	g.Run("127.0.0.1:9001")
+	
+	srv := &http.Server{
+		Addr: "127.0.0.1:9001",
+		Handler: g,
+	}
+	
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Listen: %s\n", err)
+		}
+	}()
+	
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(),5 * time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
 
 type Info struct {
@@ -27,7 +57,7 @@ type Info struct {
 }
 
 func version(c *gin.Context) {
-	c.String(http.StatusOK, "Version: V0.0.1")
+	c.String(http.StatusOK, "Version: %s", VER)
 }
 
 func home(c *gin.Context) {
@@ -39,6 +69,8 @@ func home(c *gin.Context) {
 
 func upload(c *gin.Context) {
 	h := gin.H{}
+	wd, err := os.Getwd()
+	h["wd"] = wd
 
 	file, err := c.FormFile("file")
 	if err != nil {
@@ -50,6 +82,7 @@ func upload(c *gin.Context) {
 	if err != nil {
 		log.Println("save file error: ", err)
 	}
+	h["dest_file"] = destFile
 	h["upload_file"] = file.Filename
 
 	fmt.Println("go os:", runtime.GOOS)
@@ -61,10 +94,9 @@ func upload(c *gin.Context) {
 		// ...
 	}
 
-	unzip := exec.Command("unzip", destFile, "-d", "./")
+	os.Remove("./play-gin")
+	unzip := exec.Command("unzip", destFile, "-d", ".")
 	unzip.Run()
-
-	c.JSON(http.StatusOK, h)
 
 	switch runtime.GOOS {
 	case "windows":
@@ -74,9 +106,11 @@ func upload(c *gin.Context) {
 		sc.Start()
 		// sc = exec.Command("sc", "start", "play-gin")
 		// sc.Start()
-
 	case "linux":
-		systemctl := exec.Command("systemtcl", "restart", "sycc")
-		systemctl.Start()
+		kill := exec.Command("kill", "-2", fmt.Sprintf("%d", os.Getpid()))
+		kill.Start()
 	}
+	
+	c.JSON(http.StatusOK, h)
+	
 }
