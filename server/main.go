d8b8c01fd8146d8e4b3f758ab657e55d092c4700
Acker <acker.liu@topibd.net>
2020-09-29 18:28:30 +0800

    update

--- server/main.go
+++ server/main.go
@@ -1,6 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/gin-gonic/gin"
+)
+
+type LoginInfo struct {
+	Name     string `json:"name"`
+	Password string `json:"pass"`
 }
 
+func main() {
+	srv := gin.Default()
+	srv.StaticFile("/", "web/index.html")
+
+	srv.POST("/login", login)
+
+	srv.Run(":8544")
+}
+
+func login(c *gin.Context) {
+	m := LoginInfo{}
+	if err := c.ShouldBindJSON(&m); err != nil {
+		log.Println("Login error:", err)
+	}
+
+	fmt.Println("login:", m)
+}
